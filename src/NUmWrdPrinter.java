84acb09e8c619326083126f67d49d149f1a9e2b3
Taseen Tajwar <taseentajwar95@gmail.com>
2024-09-26 11:39:38 +0600

    Creating NumWrdPrinter class

---
+++ src/NUmWrdPrinter.java
@@ -0,0 +1,32 @@
+public class NUmWrdPrinter {
+
+
+        public String printWord(int n) {
+            if (n % 2 == 0 & n % 11 == 0 & n % 13 == 0) {
+                return "evenPrimeLucky";
+            } else if (n % 2 == 0 & n % 11 == 0) {
+                return "EvenPrimeEleven";
+            } else if (n % 2 == 0) {
+                return "Even";
+            } else if (n % 11 == 0) {
+                return "PrimeEleven";
+            } else if (n % 13 == 0) {
+                return "EvenPrimeEleven";
+            } else {
+                return "OddOneOut";
+            }
+
+        }
+
+        public String printWordCap(int n) {
+            return printWord(n).toUpperCase();
+        }
+
+        public static void main(String[] args) {
+            NumWordPrinter printer = new NumWordPrinter();
+            System.out.println(printer.printWord(1));
+            System.out.println(printer.printWordCap(2));
+        }
+
+    }
+
